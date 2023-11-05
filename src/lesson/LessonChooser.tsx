22f34cd1e35c574372fcf979ca17ee6592f15ab9
Iñaki Ibarrola <inaki.ibarrola@appfire.com>
2023-11-05 20:36:05 +0100

    several lessons

--- src/lesson/LessonChooser.tsx
+++ src/lesson/LessonChooser.tsx
@@ -1,16 +1,33 @@
 import { Button } from "grommet";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import practiceData from "../data/nork-nor-orainaldia-001.json";
+
+const data = import.meta.glob("../data/*.json");
+
+const imp: LessonData[] = [];
+
+for (const path in data) {
+  data[path]().then((mod) => {
+    imp.push(mod as LessonData);
+  });
+}
+console.log("imp", imp);
 
 function LessonChooser(props: LessonChooserProps) {
   const { t } = useTranslation();
   return (
     <>
-      <h1>{t('Choose a lesson')}</h1>
-      <Button
-        label="Nork nor orainaldia"
-        onClick={() => props.onChoose(practiceData)}
-      ></Button>
+      <h1>{t("Choose a lesson")}</h1>
+      {imp.map((i, j) => {
+        return (
+          <Button
+            key={j}
+            label={i.name}
+            onClick={() => props.onChoose(i)}
+            margin="small"
+          ></Button>
+        );
+      })}
     </>
   );
 }
@@ -18,10 +35,11 @@ interface LessonChooserProps {
   onChoose: (lesson: LessonData) => void;
 }
 export interface LessonData {
+  name: string;
   exercises: ExerciseData[];
 }
 
 export interface ExerciseData {
-    type: string;
-  }
+  type: string;
+}
 export default LessonChooser;
